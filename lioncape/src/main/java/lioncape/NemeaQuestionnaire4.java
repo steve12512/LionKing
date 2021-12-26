b6ad9cb6b2a453b2c8f0656f0dd0413a32fd70ef
steve <steve@DESKTOP-M2PONU5>
2021-12-26 20:01:25 +0200

    classes 4-10

--- lioncape/src/main/java/lioncape/NemeaQuestionnaire4.java
+++ lioncape/src/main/java/lioncape/NemeaQuestionnaire4.java
@@ -1,22 +1,23 @@
 package lioncape;
 
-import java.awt.Color;
 import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import javax.swing.SwingConstants;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class NemeaQuestionnaire3 extends JFrame {
+public class NemeaQuestionnaire4 extends JFrame {
 
 	private JPanel contentPane;
 	private JLabel playerHPLabel;
@@ -28,7 +29,7 @@ public class NemeaQuestionnaire3 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					NemeaQuestionnaire3 frame = new NemeaQuestionnaire3();
+					NemeaQuestionnaire4 frame = new NemeaQuestionnaire4();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -40,7 +41,7 @@ public class NemeaQuestionnaire3 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public NemeaQuestionnaire3() {
+	public NemeaQuestionnaire4() {
 		super("Lioncape");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(300, 80, 920, 740);
@@ -50,26 +51,27 @@ public class NemeaQuestionnaire3 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Who hosted Hercules before he killed the lion?");
+		JLabel lblNewLabel = new JLabel("The Hydra emits toxic fumes that may poison you. How do you protect yourself?");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 35));
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 40));
 		lblNewLabel.setForeground(new Color(255, 215, 0));
-		lblNewLabel.setBounds(6, 47, 898, 76);
+		lblNewLabel.setBounds(6, 47, 898, 67);
 		contentPane.add(lblNewLabel);
 		
-		JButton answerButtonA = new JButton("A barman.");
+		JButton answerButtonA = new JButton("I need no protection. I am the son of Zeus!");
 		answerButtonA.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
+		answerButtonA.setBackground(Color.YELLOW);
 		answerButtonA.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (hp.getHP() <= 1) {
 					final ImageIcon icon1 = new ImageIcon("src/main/resources/SpinellisCry.png");
-					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong></html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
 					dispose();
 				} else {
 					hp.setHP(hp.getHP() - 1);
 					playerHPLabel.setText(String.valueOf(hp.getHP()));
-					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis8.png");
-					JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>BZZZZZZZZZZZZZZZZZ Try Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis2.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>BZZZZZZZZZZZZZZZZZ /nGet off your high horse Hercules!</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
 				}
 			}
 		});
@@ -77,40 +79,35 @@ public class NemeaQuestionnaire3 extends JFrame {
 		answerButtonA.setBounds(145, 542, 300, 50);
 		contentPane.add(answerButtonA);
 		
-		JButton answerButtonB = new JButton("Goddess Athena");
+		JButton answerButtonB = new JButton("I use a spare cloth to cover my mouth and nose.");
 		answerButtonB.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
 		answerButtonB.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (hp.getHP() <= 1) {
-					final ImageIcon icon1 = new ImageIcon("src/main/resources/SpinellisCry.png");
-					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
-					dispose();
-				} else {
-					hp.setHP(hp.getHP() - 1);
-					playerHPLabel.setText(String.valueOf(hp.getHP()));
-					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis4.png");
-					JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>BZZZZZZZZZZZZZZZZZ Try Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
-				}
+				final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis5.png");
+				JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>BRAVO /nThe toxic particles are blocked!</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+				dispose();
+				NemeaQuestionnaire2 nemea2 = new NemeaQuestionnaire2();
+				nemea2.setVisible(true);
 			}
 		});
 		answerButtonB.setForeground(new Color(100, 149, 237));
 		answerButtonB.setBounds(469, 542, 300, 50);
 		contentPane.add(answerButtonB);
 		
-		JButton answerButtonC = new JButton("A woodworker.");
+		JButton answerButtonC = new JButton("I do not come close to the Hydra and I try to hit it with a bow");
 		answerButtonC.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
 		answerButtonC.setBackground(new Color(255, 255, 255));
 		answerButtonC.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (hp.getHP() <= 1) {
 					final ImageIcon icon1 = new ImageIcon("src/main/resources/SpinellisCry.png");
-					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong></html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
 					dispose();
 				} else {
 					hp.setHP(hp.getHP() - 1);
 					playerHPLabel.setText(String.valueOf(hp.getHP()));
-					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis2.png");
-					JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>BZZZZZZZZZZZZZZZZZ Try Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis4.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>The Hydra’s scales break your arrows in half./n Try Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
 				}
 			}
 		});
@@ -118,27 +115,32 @@ public class NemeaQuestionnaire3 extends JFrame {
 		answerButtonC.setBounds(145, 623, 300, 50);
 		contentPane.add(answerButtonC);
 		
-		JButton answerButtonD = new JButton("A shepherd.");
+		JButton answerButtonD = new JButton("I hold my breath.");
 		answerButtonD.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
 		answerButtonD.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis5.png");
-				JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>BRAVO</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
-				dispose();
-				NemeaQuestionnaire4 nemea4 = new NemeaQuestionnaire4();
-				nemea4.setVisible(true);
+				if (hp.getHP() <= 1) {
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/SpinellisCry.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong></html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					dispose();
+				} else {
+					hp.setHP(hp.getHP() - 1);
+					playerHPLabel.setText(String.valueOf(hp.getHP()));
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis3.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>Diomodis Spinellis said:</Strong><br><br>Humans HAVE to breathe sometimes Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+				}
 			}
 		});
 		answerButtonD.setForeground(new Color(100, 149, 237));
 		answerButtonD.setBounds(469, 623, 300, 50);
 		contentPane.add(answerButtonD);
 		
-		JLabel NemeaLabel_3 = new JLabel("");
-		NemeaLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel NemeaLabel_1 = new JLabel("");
+		NemeaLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		Image img = new ImageIcon(this.getClass().getResource("/NemeaImg.png")).getImage();
-		NemeaLabel_3.setIcon(new ImageIcon(img));
-		NemeaLabel_3.setBounds(6, 107, 918, 420);
-		contentPane.add(NemeaLabel_3);
+		NemeaLabel_1.setIcon(new ImageIcon(img));
+		NemeaLabel_1.setBounds(6, 107, 918, 420);
+		contentPane.add(NemeaLabel_1);
 		
 		playerHPLabel = new JLabel("");
 		playerHPLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
@@ -155,6 +157,4 @@ public class NemeaQuestionnaire3 extends JFrame {
 		HealthPowerLabel.setBounds(6, 14, 116, 31);
 		contentPane.add(HealthPowerLabel);
 	}
-
 }
-
---
+++ lioncape/src/main/java/lioncape/NemeaQuestionnaire6.java
@@ -0,0 +1,159 @@
+package lioncape;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class NemeaQuestionnaire6 extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel playerHPLabel;
+	HealthPower hp = new HealthPower();
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NemeaQuestionnaire5 frame = new NemeaQuestionnaire5();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NemeaQuestionnaire6() {
+		super("Lioncape");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(300, 80, 920, 740);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("You defeated the monster! How will you prove it?");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 40));
+		lblNewLabel.setForeground(new Color(255, 215, 0));
+		lblNewLabel.setBounds(6, 47, 898, 67);
+		contentPane.add(lblNewLabel);
+		
+		JButton answerButtonD = new JButton("I will dip my weapons into its poisonous blood");
+		answerButtonD.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
+		answerButtonD.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis6.png");
+				JOptionPane.showMessageDialog(null, "<html><Your arrows are now poisonous!Your arrows are now poisonous! Smart!>BRAVO</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+				dispose();
+				NemeaQuestionnaire7 nemea7 = new NemeaQuestionnaire7();
+				nemea7.setVisible(true);
+			}
+		});
+		answerButtonD.setForeground(new Color(100, 149, 237));
+		answerButtonD.setBounds(145, 542, 300, 50);
+		contentPane.add(answerButtonD);
+		
+		JButton answerButtonA = new JButton("I’ll carry one of its heads with me");
+		answerButtonA.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
+		answerButtonA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (hp.getHP() <= 1) {
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/SpinellisCry.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong></html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					dispose();
+				} else {
+					hp.setHP(hp.getHP() - 1);
+					playerHPLabel.setText(String.valueOf(hp.getHP()));
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis9.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>Too heavy /nTry Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+				}
+			}
+		});
+		answerButtonA.setForeground(new Color(100, 149, 237));
+		answerButtonA.setBounds(469, 542, 300, 50);
+		contentPane.add(answerButtonA);
+		
+		JButton answerButtonC = new JButton("I will just leave, its corpse might be dangerous");
+		answerButtonC.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
+		answerButtonC.setBackground(new Color(255, 255, 255));
+		answerButtonC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (hp.getHP() <= 1) {
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/SpinellisCry.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong></html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					dispose();
+				} else {
+					hp.setHP(hp.getHP() - 1);
+					playerHPLabel.setText(String.valueOf(hp.getHP()));
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis8.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>Ugh. Wasted potentialTry Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+				}
+			}
+		});
+		answerButtonC.setForeground(new Color(100, 149, 237));
+		answerButtonC.setBounds(145, 623, 300, 50);
+		contentPane.add(answerButtonC);
+		
+		JButton answerButtonB = new JButton("I will eat it to gain its power.       ");
+		answerButtonB.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
+		answerButtonB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (hp.getHP() <= 1) {
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/SpinellisCry.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>GAME OVER</Strong></html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+					dispose();
+				} else {
+					hp.setHP(hp.getHP() - 1);
+					playerHPLabel.setText(String.valueOf(hp.getHP()));
+					final ImageIcon icon1 = new ImageIcon("src/main/resources/Spinellis7.png");
+					JOptionPane.showMessageDialog(null, "<html><Strong>Eating poison? Yum -_-!\nTry Again</html>", "Message", + JOptionPane.INFORMATION_MESSAGE, icon1);
+				}
+			}
+		});
+		answerButtonB.setForeground(new Color(100, 149, 237));
+		answerButtonB.setBounds(469, 623, 300, 50);
+		contentPane.add(answerButtonB);
+		
+		JLabel NemeaLabel_2 = new JLabel("");
+		NemeaLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		Image img = new ImageIcon(this.getClass().getResource("/NemeaImg.png")).getImage();
+		NemeaLabel_2.setIcon(new ImageIcon(img));
+		NemeaLabel_2.setBounds(6, 107, 918, 420);
+		contentPane.add(NemeaLabel_2);
+		
+		playerHPLabel = new JLabel("");
+		playerHPLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		playerHPLabel.setText(String.valueOf(hp.getHP()));
+		playerHPLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		playerHPLabel.setForeground(new Color(255, 255, 255));
+		playerHPLabel.setBounds(112, 14, 30, 31);
+		contentPane.add(playerHPLabel);
+		
+		JLabel HealthPowerLabel = new JLabel("Health Power:");
+		HealthPowerLabel.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
+		HealthPowerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		HealthPowerLabel.setForeground(new Color(255, 255, 255));
+		HealthPowerLabel.setBounds(6, 14, 116, 31);
+		contentPane.add(HealthPowerLabel);
+	}
+
+}
